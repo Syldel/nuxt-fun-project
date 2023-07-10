5fa6f787930037eb64fea8eaa5dbbc36eeae622e
Syl D <sdelescluse@atecna.fr>
2023-07-10 11:07:05 +0200

    chore(style): media sass mixins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,13 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/assets/style/global.sass"',
+        },
+      },
+    },
+  },
 })
