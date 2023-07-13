d9ee594b90ad4af7d15f4e2e65cd71b4cdcd3dd4
Syl D <sdelescluse@atecna.fr>
2023-07-13 11:29:54 +0200

    feat(components): Dark Mode Toggle

---
+++ components/DarkModeToggle.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+  },
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['change'])
+
+const checkedRef = ref(props.checked)
+
+function handleSpace() {
+  checkedRef.value = !checkedRef.value
+  emit('change', checkedRef.value)
+}
+
+function handleClick() {
+  emit('change', !checkedRef.value)
+}
+</script>
+
+<template>
+  <label :tabindex="disabled ? undefined : 0" :class="{ disabled }" @keypress.space.prevent="handleSpace">
+    <input :id="id" v-model="checkedRef" type="checkbox" :disabled="disabled">
+    <span class="slider" @click="handleClick" />
+  </label>
+</template>
+
+<style lang="sass">
+:root
+  --light: #d8dbe0
+  --dark: #28292c
+</style>
+
+<style lang="sass" scoped>
+$ratioSize: 0.33
+
+label
+  position: relative
+  display: block
+  width: calc(200px * $ratioSize)
+  height: calc(100px * $ratioSize)
+  background-color: var(--dark)
+  border-radius: 50px
+  cursor: pointer
+
+  &.disabled
+    opacity: 0.3
+
+input
+  position: absolute
+  display: none
+
+  &:checked ~ .slider
+    background-color: var(--light)
+
+    &::before
+      transform: translateX(calc(95px* $ratioSize))
+      background-color: var(--dark)
+      box-shadow: none
+
+.slider
+  position: absolute
+  width: 100%
+  height: 100%
+  border-radius: calc(50px * $ratioSize)
+  transition: 0.3s
+
+  &::before
+    content: ""
+    position: absolute
+    top: calc(13px * $ratioSize)
+    left: calc(16px * $ratioSize)
+    width: calc(75px * $ratioSize)
+    height: calc(75px * $ratioSize)
+    border-radius: 50%
+    box-shadow: inset calc(28px* $ratioSize) calc(-4px* $ratioSize) 0px 0px var(--light)
+    background-color: var(--dark)
+    transition: 0.3s
+</style>
